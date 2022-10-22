1f74a68809c8f3d428fe7cdd9d1f4d33242d9eea
Israfil <mdronnieronniers@gmial.com>
2022-10-23 01:27:12 +0600

    vercel deploy done

--- index.js
+++ index.js
@@ -6,12 +6,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
 	res.send('hello from behind');
 });
 app.get('/news-categories', (req, res) => {
 	res.send(categories);
+});
+
+app.get('/news/:id', (req, res) => {
+	const id = req.params.id;
+	const selectedNews = news.find((news) => news._id === id);
+	res.send(selectedNews);
+});
+app.get('/category/:id', (req, res) => {
+	const id = req.params.id;
+	if (id === '08') {
+		res.send(news);
+	} else {
+		const selectedCategory = news.filter((news) => news.category_id === id);
+		res.send(selectedCategory);
+	}
 });
 
 app.listen(port, () => {
